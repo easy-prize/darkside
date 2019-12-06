5637a86046a1db7854bcd12dd96f46344ae277ac
Joosung park <admin@slowmotion.dev>
2019-12-06 23:45:45 +0900

    Add: Swagger OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
-import { config } from '@app/config';
+import { config, NodeEnv } from '@app/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
+
+  if (config.nodeEnv === NodeEnv.dev) {
+    const options = new DocumentBuilder()
+      .setTitle('Easy Prize')
+      .setDescription('Easy Prize API description')
+      .setVersion(process.env.npm_package_version)
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('docs/swagger', app, document);
+  }
+
   await app.listen(config.port, config.host);
 };
 
