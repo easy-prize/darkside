667e31b4bb72936d457c35cbc8480023bbbd492c
Joosung Park <admin@slowmotion.dev>
2019-12-06 12:08:32 +0900

    Add: TSLint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly hello = 'Hello World!';
+
+  public getHello(): string {
+    return this.hello;
   }
 }
