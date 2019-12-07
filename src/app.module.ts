7b0a01cb6c41cb6c57c3e13bde239ee9b8d724b8
Joosung park <admin@slowmotion.dev>
2019-12-07 11:32:17 +0900

    Remove: Imports in App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
-import { MongoModule } from '@app/mongo';
-import { PushModule } from '@app/push';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   controllers: [AppController],
-  imports: [MongoModule, PushModule],
+  imports: [],
   providers: [AppService],
 })
 export class AppModule {
